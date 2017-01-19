f13fdc82679618660aca1cf9dea9754d98b7b78c
Andrew Combs <andrewcombs13@gmail.com>
2017-01-19 16:40:57 -0500

    Implement the turtle itself, and all the math and logic it needs. Next up need to add graphics/drawing the turtle's path and all the control logic in MainWindow.cs

---
+++ TurtleGraphics/Turtle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurtleGraphics
+{
+    enum StepSize { Nintey = 90, Sixty = 60, FourtyFive = 45, Thirty = 30, Ten = 10 }
+    class Turtle
+    {
+        private const int FORWARD_DISTANCE = 20;
+        private const StepSize stepSize = StepSize.Thirty;
+
+        private TurtleState currentState;
+
+        public Turtle()
+        {
+            currentState = new TurtleState() { XCoord = 0, YCoord = 0 };
+        }
+
+        public Tuple<int, int> GetLocation()
+        {
+            return new Tuple<int, int>(currentState.XCoord, currentState.YCoord);
+        }
+
+        public void TurnRight()
+        {
+            currentState.Direction = currentState.Direction - (int)stepSize;
+        }
+
+        public void TurtLeft()
+        {
+            currentState.Direction = currentState.Direction + (int)stepSize;
+        }
+
+        public Tuple<int, int> GoForward()
+        {
+            double dirRad = (double)currentState.Direction * Math.PI / 180.0;
+            int xDist = (int)(FORWARD_DISTANCE * Math.Sin(dirRad));
+            int yDist = (int)(FORWARD_DISTANCE * Math.Cos(dirRad));
+
+            currentState.XCoord += xDist;
+            currentState.YCoord += yDist;
+
+            return new Tuple<int, int>(currentState.XCoord, currentState.YCoord);
+        }
+
+        public void SaveState()
+        {
+            TurtleState newState = new TurtleState();
+            newState.Direction = currentState.Direction;
+            newState.XCoord = currentState.XCoord;
+            newState.YCoord = currentState.YCoord;
+            newState.PreviousState = currentState;
+            currentState = newState;
+        }
+
+        public void RecallState()
+        {
+            TurtleState newState = new TurtleState();
+            newState.Direction = currentState.PreviousState.Direction;
+            newState.XCoord = currentState.PreviousState.XCoord;
+            newState.YCoord = currentState.PreviousState.YCoord;
+            newState.PreviousState = currentState.PreviousState;
+            currentState = newState;
+        }
+    }
+}
---
+++ TurtleGraphics/TurtleState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace TurtleGraphics
+{
+    class TurtleState
+    {
+        private int direction = 0;
+
+        public int Direction
+        {
+            get { return direction; }
+            set
+            {
+                direction = value;
+                // Check for overflow
+                direction = (direction >= 360) ? direction - 360 : direction;
+                // Chcek for underflow
+                direction = (direction < 0) ? direction + 360 : direction;
+            }
+        }
+        public int XCoord { get; set; } = 0;
+        public int YCoord { get; set; } = 0;
+
+        public TurtleState PreviousState { get; set; } = null;
+    }
+}
